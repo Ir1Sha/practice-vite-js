55bc59603b58f97ddd326359912feb1bff0eea31
Anna Bieliavtseva <belyavtseva1911@gmail.com>
2024-12-08 12:04:45 +0100

    add loader

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -3,15 +3,16 @@ import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.min.css';
 import renderingFoo from './renderfoo.js';
 import iziToast from 'izitoast';
-import icon from "../img/javascript.svg";
-import "izitoast/dist/css/iziToast.min.css";
+import icon from '../img/javascript.svg';
+import 'izitoast/dist/css/iziToast.min.css';
 
 const api = new UnsplashAPI();
 
 const galleryList = document.querySelector('.gallery');
 const container = document.getElementById('tui-pagination-container');
-const form = document.querySelector(".js-search-form");
-const input = document.querySelector(".search-input");
+const form = document.querySelector('.js-search-form');
+const input = document.querySelector('.search-input');
+const loader = document.querySelector('.loader');
 
 const options = {
   totalItems: 0,
@@ -29,27 +30,47 @@ api.getPopularPhotos(currentPage).then(result => {
   pagination.reset(result.total);
 });
 
-pagination.on('afterMove', event => {
+pagination.on('afterMove', onPaginationOn);
+
+function onPaginationOn(event) {
   const currentPage = event.page;
   api
     .getPopularPhotos(currentPage)
     .then(res => (galleryList.innerHTML = renderingFoo(res.results)));
-});
+}
 
-form.addEventListener("submit", (event) =>{
+form.addEventListener('submit', event => {
   event.preventDefault();
   const inputValue = input.value.trim();
-  if (inputValue === "") {
-    iziToast.info({message: 'Enter smt for search',
-      iconUrl: icon
-    });
+  if (inputValue === '') {
+    iziToast.info({ message: 'Enter smt for search', iconUrl: icon });
     return;
-  } 
+  }
   api.query = inputValue;
+  pagination.off('afterMove', onPaginationOn);
+  pagination.off('afterMove', onPaginationSearchByQuery);
 
-  api.getPhotosByQuery(currentPage)
-  .then(result => {
-    galleryList.innerHTML = renderingFoo(result.results);
-    pagination.reset(result.total);
-  });
-})
+  loader.classList.remove('hidden');
+
+  api
+    .getPhotosByQuery(currentPage)
+    .then(result => {
+      galleryList.innerHTML = renderingFoo(result.results);
+
+      pagination.reset(result.total);
+    })
+    .catch(error => console.log(error))
+    .finally(() => {
+      loader.classList.add('hidden');
+      form.reset();
+    });
+
+  pagination.on('afterMove', onPaginationSearchByQuery);
+});
+
+function onPaginationSearchByQuery(event) {
+  const currentPage = event.page;
+  api
+    .getPhotosByQuery(currentPage)
+    .then(res => (galleryList.innerHTML = renderingFoo(res.results)));
+}
