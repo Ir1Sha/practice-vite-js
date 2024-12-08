845ec6c879c50b2b05c731094e2c4b10d229b63a
maskaa22 <kyrosika565@gmail.com>
2024-12-08 12:09:10 +0200

    pagination

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,10 +1,32 @@
 import UnsplashAPI from './UnsplashAPI.js';
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.min.css';
 import renderingFoo from './renderfoo.js';
 
 const api = new UnsplashAPI();
 
 const galleryList = document.querySelector('.gallery');
+const container = document.getElementById('tui-pagination-container');
 
-api
-  .getPopularPhotos(1)
-  .then(result => (galleryList.innerHTML = renderingFoo(result.results)));
+const options = {
+  totalItems: 0,
+  itemsPerPage: 12,
+  visiblePages: 5,
+  page: 1,
+};
+
+const pagination = new Pagination(container, options);
+
+const currentPage = pagination.getCurrentPage();
+
+api.getPopularPhotos(currentPage).then(result => {
+  galleryList.innerHTML = renderingFoo(result.results);
+  pagination.reset(result.total);
+});
+
+pagination.on('afterMove', event => {
+  const currentPage = event.page;
+  api
+    .getPopularPhotos(currentPage)
+    .then(res => (galleryList.innerHTML = renderingFoo(res.results)));
+});
